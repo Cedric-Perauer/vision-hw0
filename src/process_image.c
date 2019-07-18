9330d41ff400b4b6ded77711dbd28f0cf6b24624
Cedric Perauer <40869163+Cedric-Perauer@users.noreply.github.com>
2019-07-18 20:51:36 +0200

    Update process_image.c

--- src/process_image.c
+++ src/process_image.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <string.h>
 #include <assert.h>
 #include <math.h>
@@ -6,19 +6,63 @@
 
 float get_pixel(image im, int x, int y, int c)
 {
-    // TODO Fill this in
-    return 0;
+    if( x < 0){
+        x = 0;
+    }
+    if( y < 0){
+        y = 0;
+    }
+    if(x >= im.w){
+  x= im.w-1;
+    }
+      if(y >= im.h){
+  y= im.h-1;
+    }
+    if(c < 0){
+        c = 0;
+
+    }
+    if(c > 2){
+        c = 2;
+    }
+    return im.data[(c*im.w*im.h)+(y*im.w)+(x)];
 }
 
+ 
 void set_pixel(image im, int x, int y, int c, float v)
-{
-    // TODO Fill this in
+{ 
+   if( x < 0){
+        x = 0;
+    }
+    if( y < 0){
+        y = 0;
+    }
+    if(x >= im.w){
+  x= im.w-1;
+    }
+      if(y >= im.h){
+  y= im.h-1;
+    }
+    if(c < 0){
+        c = 0;
+
+    }
+    if(c > 2){
+        c = 2;
+    }
+  
+  im.data[(c*im.w*im.h)+(y*im.w)+(x)] = v;
+
 }
+
 
 image copy_image(image im)
 {
     image copy = make_image(im.w, im.h, im.c);
-    // TODO Fill this in
+    for(int i = 0; i < im.h*im.w*im.c; i++){
+
+        copy.data[i] = im.data[i];
+    }
     return copy;
 }
 
@@ -26,18 +70,30 @@ image rgb_to_grayscale(image im)
 {
     assert(im.c == 3);
     image gray = make_image(im.w, im.h, 1);
-    // TODO Fill this in
+    for(int i = 0; i < im.h*im.w; i++){
+        gray.data[i] = im.data[i] * 0.299 + im.data[i+(im.h*im.w)] * 0.587  + 0.114 * im.data[i+(2*im.h*im.w)];
+    }
     return gray;
 }
 
 void shift_image(image im, int c, float v)
 {
-    // TODO Fill this in
+    for(int i = 0; i < im.h*im.w; i++){
+        im.data[(c*im.h*im.w)+i] += v;
+    }
 }
 
 void clamp_image(image im)
-{
-    // TODO Fill this in
+{ 
+    for(int i = 0; i < im.c*im.h*im.w; i++){
+        if(im.data[i] < 0){
+            im.data[i] = 0;
+        } 
+        if(im.data[i] > 1){
+            im.data[i] = 1;
+        } 
+    }
+       
 }
 
 
